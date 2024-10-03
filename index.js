d2961e61dcb0d0491857e4e8af2d2b96ea987ccb
EllieMott <elizabethmott1326@gmail.com>
2024-10-03 08:12:19 -0400

    +empty functions in .js

---
+++ index.js
@@ -0,0 +1,43 @@
+function btn1(){}
+
+function btn2(){}
+
+function btn3(){}
+
+function btn4(){}
+
+function btn5(){}
+
+function btn6(){}
+
+function btn7(){}
+
+function btn8(){}
+
+function btn9(){}
+
+function btnDec(){}
+
+function btnAdd(){}
+
+function btnSub(){}
+
+function btnMult(){}
+
+function btnDivi(){}
+
+function btnEql(){}
+
+function btnPerc(){}
+
+function btnSqr(){}
+
+function btnSqrRt(){}
+
+function btnSgn(){}
+
+function btnBack(){}
+
+function btnClr(){}
+
+function btnClrRslt(){}
